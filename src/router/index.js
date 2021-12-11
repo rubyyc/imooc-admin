052b3fa00bfe30fb818a7b2add71e0e5566c0549
YuanChang <759337828@qq.com>
2021-12-11 20:18:58 +0800

    feat: feat: 引入element-plus

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-
-const routes = [
-]
+import {
+  createRouter,
+  createWebHashHistory
+} from 'vue-router'
+// 公开路由表
+const publicRoutes = [{
+  path: '/login',
+  // 懒加载
+  component: () => import('@/views/login/index')
+}]
 
 const router = createRouter({
   history: createWebHashHistory(),
-  routes
+  routes: publicRoutes
 })
 
 export default router
---
+++ src/views/login/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="login-container">
+    <el-form class="login-form">
+      <div class="title-container">
+        <h3 class="title">用户登录</h3>
+      </div>
+      <el-form-item>
+        <span class="svg-container">
+          <el-icon>
+            <avatar />
+          </el-icon>
+        </span>
+        <el-input placeholder="username" name="username" type="text"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <span class="svg-container">
+          <el-icon>
+            <avatar />
+          </el-icon>
+        </span>
+        <el-input placeholder="password" name="password"></el-input>
+        <span class="show-pwd">
+          <el-icon>
+            <avatar />
+          </el-icon>
+        </span>
+      </el-form-item>
+      <el-button type="primary" style="width:100%;margin-bottom:30px;" @click="onSubmit">立即创建</el-button>
+      <el-button>取消</el-button>
+    </el-form>
+  </div>
+</template>
+<script setup>
+import { Avatar } from '@element-plus/icons'
+import { } from 'vue'
+</script>
+<style lang="scss" scoped>
+</style>
