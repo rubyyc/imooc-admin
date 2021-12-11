b3000aa16d64617688557f79b07632d24c9ee6b6
YuanChang <759337828@qq.com>
2021-12-11 21:24:46 +0800

    feat: 自定义icon组件

---
+++ src/components/SvgIcon/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <!-- 展示外部图标 -->
+  <div class></div>
+  <!-- 展示内部图标 -->
+</template>
+<script setup>
+import { defineProps } from 'vue'
+defineProps({
+  // icon 图标
+  icon: {
+    type: String,
+    required: true
+  },
+  // 图标类名
+  className: {
+    type: String,
+    default: ''
+  }
+})
+
+/**
+ * 判断当前图标是否为外部图标
+ */
+</script>
+<style lang="scss" scoped>
+</style>
