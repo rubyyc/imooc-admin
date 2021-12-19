4e16c18e6540a9a76ff59f4e25f11ae8b1287207
YuanChang <759337828@qq.com>
2021-12-19 14:59:33 +0800

    feat: 自定义切换语言的组件

---
+++ src/components/LangSelect/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <el-dropdown trigger="click" class="international" @command="handleSetLanguage">
+    <div>
+      <el-tooltip content="国际化" :effect="effect">
+        <svg-icon icon="language" />
+      </el-tooltip>
+    </div>
+    <template #dropdown>
+      <el-dropdown-menu>
+        <el-dropdown-item :disabled="language === 'zh'" command="zh">中文</el-dropdown-item>
+        <el-dropdown-item :disabled="language === 'en'" command="en">English</el-dropdown-item>
+      </el-dropdown-menu>
+    </template>
+  </el-dropdown>
+</template>
+<script setup>
+import { useStore } from 'vuex'
+import { computed, defineProps } from 'vue'
+import { useI18n } from 'vue-i18n'
+import { ElMessage } from 'element-plus'
+
+defineProps({
+  effect: {
+    type: String,
+    default: 'dark',
+    validator: function (value) {
+      return ['dark', 'light'].indexOf(value) !== -1
+    }
+  }
+})
+
+const i18n = useI18n()
+const store = useStore()
+const language = computed(() => {
+  return store.getters.language
+})
+
+const handleSetLanguage = (lang) => {
+  i18n.locale.value = lang
+  store.commit('app/setLanguage', lang)
+  ElMessage.success('更新成功')
+}
+</script>
+<style lang="scss" scoped>
+</style>
--- src/store/modules/app.js
+++ src/store/modules/app.js
@@ -1,11 +1,23 @@
+import {
+	getItem,
+	setItem
+} from '@/utils/storage'
+import {
+	LANG
+} from '@/constant'
 export default {
 	namespaced: true,
 	state: () => ({
-		sidebarOpened: true
+		sidebarOpened: true,
+		language: getItem(LANG) || 'zh'
 	}),
 	mutations: {
 		triggerSidebarOpened(state) {
 			state.sidebarOpened = !state.sidebarOpened
+		},
+		setLanguage(state, language) {
+			setItem(LANG, language)
+			state.language = language
 		}
 	}
 }
